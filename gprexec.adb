c203e88c52c11e16b0ef63445d1ed1681811df95
Vincent Celier <vmcelier@gmail.com>
2019-01-11 21:16:01 -0800

    Add preambles to source files and switch -gnatg

--- gprexec.adb
+++ gprexec.adb
@@ -1,3 +1,17 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                             GPREXEC                                      --
+--                                                                          --
+--                   Copyright (C) 2018, Vincent Celier                     --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+------------------------------------------------------------------------------
+
 with Ada.Text_IO; use Ada.Text_IO;
 
 with GPR.Attr;    use GPR.Attr;
